d80d50a1e40ace2918d9b01076abba99e9470678
MaxCau <maxime.caudrelier@etu.umontpellier.fr>
2023-06-12 10:02:40 +0200

    First commit

---
+++ STM32/app/src/log.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+void create_log_file ()
+{
+FILE* log = NULL;
+log = fopen("SACS_log.log", "w");
+if (log != NULL)
+{
+fputs("Génération du fichier log pour le protocole SACS", log);
+fclose(log);
+}
+}
+
+void add_log_input ()
+{
+    
+}
